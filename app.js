f8d85e9bc5ea89f9496073b919d5563695e855bd
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-28 17:50:02 +0530

    app: use localtunnel in code

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 require('dotenv')
+const lt = require('localtunnel')
 const express = require('express')
-
 const logger = require('./utils/logger')
+logger.info(`NODE_ENV: ${process.env.NODE_ENV}`)
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
+
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/', (_, res) => res.json({ message: "Done!"}))
 
+if (process.env.NODE_ENV === 'development') {
+    lt({ port: 3000, subdomain: process.env.LT_SUBDOMAIN }, (err, tunnel) => logger.info(`Localtunnel Started: ${tunnel.url}`))
+}
 app.listen(PORT, logger.info(`Server started on port ${PORT}`))
