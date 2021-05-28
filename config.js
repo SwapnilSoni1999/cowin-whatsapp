a83d6f5533591c55f35dac1165bdb4b315e95575
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-28 20:28:28 +0530

    config: create whatsapp client

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 const path = require('path')
+const WhatsApp = require('./lib/whatsapp')
+
+const whatsapp = new WhatsApp(process.env.NODE_ENV === 'development' ? process.env.API_KEY_TESTING : process.env.API_KEY_AWS)
 
 module.exports = {
     dirs: {
         assets: path.resolve('assets'),
         certificate: path.resolve('assets', 'certificates')
-    }
+    },
+    whatsapp
 }
