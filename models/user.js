c23ef180f0c367b950abd6e77e8e961885a2d57c
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-28 20:27:53 +0530

    model: create user schema

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose')
+
+const UserSchema = new Schema({
+    number: { type: Number, unique: true, required: true },
+    state: { type: Object, default: {} },
+    txnId: { type: String, default: null },
+    token: { type: String, default: null }
+})
+
+const User = model('User', UserSchema, 'users')
+
+module.exports = User
