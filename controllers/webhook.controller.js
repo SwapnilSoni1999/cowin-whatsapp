a5cb8415995f4eb7c054c47caae8a26ed9f02c99
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-28 17:08:39 +0530

    controller: webhook: add empty message checker

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -2,6 +2,9 @@ const Whatsapp = require('../lib/whatsapp')
 
 class WebHook {
     static async handle(req, res) {
+        if (!req.body.messages.length) {
+            return res.end() 
+        }
         const messages = req.body.messages
         for (const message of messages) {
             const waId = message.from
