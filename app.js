4890c681a59f40e15ec9452a2e93be071da8f255
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-29 00:04:41 +0530

    fix typo in app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const logger = require('./utils/logger')
+const webhookRoute = require('./routes/webhook.route')
 
 logger.info(`NODE_ENV: ${process.env.NODE_ENV}`)
 
@@ -9,7 +10,6 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/', (_, res) => res.json({ message: "Done!"}))
-app.post('/', require('./routes/webhook.route'))
+app.use('/webhook', webhookRoute)
 
 module.exports = app
