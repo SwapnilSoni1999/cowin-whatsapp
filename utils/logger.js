054de7ca7c50b0f0ca513f614ef813512dd7ed00
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-28 21:54:00 +0530

    utils: loggers: added console and log transports

--- utils/logger.js
+++ utils/logger.js
@@ -12,7 +12,13 @@ const logger = createLogger({
             filename: `${package.name}-${package.version}-error.log`,
             level: 'error',
             format: format.combine(format.timestamp(), format.json(), format.colorize(), format.cli())
-        })
+        }),
+        new transports.File({
+            filename: `${package.name}-${package.version}-log.log`,
+            level: 'log',
+            format:format.combine(format.timestamp(), format.json(), format.colorize(), format.cli())
+        }),
+        new transports.Console({ format: format.combine(format.timestamp(), format.json(), format.colorize(), format.cli()) })
     ]
 })
 
