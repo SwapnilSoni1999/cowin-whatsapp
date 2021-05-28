9ca82cd673320470983509c11178d76980bc3c06
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-28 17:06:25 +0530

    create webhook controller

---
+++ controllers/webhook.controller.js
@@ -0,0 +1,18 @@
+const Whatsapp = require('../lib/whatsapp')
+
+class WebHook {
+    static async handle(req, res) {
+        const messages = req.body.messages
+        for (const message of messages) {
+            const waId = message.from
+            const message = message.text.body || message.button.text
+            if (!message) {
+                return res.end()
+            }
+            Whatsapp.handleMessage(waId, message)
+            return res.end()
+        }
+    }
+}
+
+module.exports = WebHook
