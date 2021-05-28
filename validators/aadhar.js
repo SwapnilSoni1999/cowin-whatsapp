6be5ff6fe71a9bc86bf3f92ff37694675b8e9d74
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-28 21:39:10 +0530

    validators: aadhar: removed unecessary checks

--- validators/aadhar.js
+++ validators/aadhar.js
@@ -4,15 +4,6 @@ const aadhar = (aadhar) => {
     if (process.env.NODE_ENV === 'development') {
         return true
     }
-
-    if (isNaN(aadhar)) {
-        return false
-    }
-
-    if (aadhar.length != 12) {
-        return false
-    }
-
     return aadharValidator.isValidNumber(aadhar)
 }
 
