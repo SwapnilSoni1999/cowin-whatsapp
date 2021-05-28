9ad3185f09a1d1880fc704235d05ccf0183dcb21
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-29 00:04:02 +0530

    webhook: send 200 status

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -1,20 +1,20 @@
-// const { whatsapp } = require('../app')
+const whatsapp = require('../lib/whatsapp')
 
 class WebHook {
     static async handle(req, res) {
         console.log(req.body)
         if (!req.body.messages.length) {
-            return res.end() 
+            return res.status(200).end() 
         }
         const messages = req.body.messages
         for (const message of messages) {
             const waId = message.from
             const msg = message.text.body || message.button.text
             if (!msg) {
-                return res.end()
+                return res.status(200).end()
             }
             whatsapp.handleMessage(waId, msg.toString().trim())
-            return res.end()
+            return res.status(200).end()
         }
     }
 }
