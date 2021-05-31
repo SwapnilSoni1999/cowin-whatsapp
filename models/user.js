53c8afb4e20e001d123e20793f6cd9f002315957
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-31 13:15:57 +0530

    model: define state and step

--- models/user.js
+++ models/user.js
@@ -2,10 +2,11 @@ const { Schema, model } = require('mongoose')
 
 const UserSchema = new Schema({
     number: { type: Number, unique: true, required: true },
-    state: { type: Object, default: {} },
+    step: { type: Number, default: null },
     txnId: { type: String, default: null },
     token: { type: String, default: null },
-    lang: { type: String, default: 'en' }
+    lang: { type: String, default: 'en' },
+    state: { type: String, default: null }
 })
 
 const User = model('User', UserSchema, 'users')
