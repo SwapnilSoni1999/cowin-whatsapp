8a0660966fd9af92ab21dcfe1de3542c8f8c2bd1
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-31 13:12:02 +0530

    Revert "[fix] validators: date: check today\'s date"
    This reverts commit ee70fbe2981c23d41c84d8621e2659d647a3c607.

--- validators/date.js
+++ validators/date.js
@@ -5,8 +5,7 @@ const isFuture = (date) => {
     if (!dateObj.isValid()) {
         throw new Error('Invalid date.')
     }
-    const yesterday = dateObj.subtract(1, 'days')
-    return (parseInt(dateObj.valueOf() / 1000)) > parseInt(moment.now() / 1000) || dateObj.isAfter(yesterday)
+    return (parseInt(dateObj.valueOf() / 1000)) > parseInt(moment.now() / 1000)
 }
 
 module.exports = isFuture
